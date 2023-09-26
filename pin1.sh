46df1f4b84a4d2b6e89bec4ef770e55910ee0776
Gonzalo Mahserdjian <gsmx64@outlook.com>
2023-09-25 23:39:25 -0300

    Adding docker-compose.yml & pin1.sh for PIN1 v1.4

--- pin1.sh
+++ pin1.sh
@@ -2,9 +2,12 @@
 
 mkdir jenkins-data/
 mkdir nexus-data/
-chown -R 200 nexus-data/
+chown -R 200:200 nexus-data/
 mkdir registry-data/
 
 docker compose up
 
+docker stop jenkins-GRUPO4
+docker stop nexus-GRUPO4
+docker stop registry-GRUPO4
 
